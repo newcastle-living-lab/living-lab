3df0af3fba05519834298f5d7e8460d7b2358260
Craig Rodway <craig@clouddataservice.co.uk>
2017-09-11 16:49:00 +0100

    Store: removed redundant code. Added code for playMode

--- src/assets/js/components/present_store.js
+++ src/assets/js/components/present_store.js
@@ -31,6 +31,8 @@ var Store = Mn.Object.extend({
 	//array of present event objects
 	presentevents: [],
 
+	playMode: false,
+
 	initialize: function() {
 
 		var self = this;
@@ -71,124 +73,14 @@ var Store = Mn.Object.extend({
 
 	},
 
-	/*
-	setupGroups: function() {
-
-		var self = this;
-
-		this.groupCollection.add({
-			name: "default"
-		});
-
-		this.listenTo(this._appChannel, "group:add", function() {
-
-			var newGroup = self.groupCollection.createNew();
-			if (newGroup) {
-				// Trigger event to say a new view has been added.
-				// Event listeners to this should make sure the views are added to the presentevents and are then updated over IO.
-				self._appChannel.trigger("group:added", {
-					group: newGroup
-				});
-			} else {
-				self._appChannel.request("ui:error", {
-					message: "Unable to create new group."
-				});
-			}
-		});
-
+	setPlayMode: function(playMode) {
+		console.log("Store | setPlayMode | " + playMode);
+		this.playMode = playMode;
 	},
-	*/
 
-	setupViewEvents: function() {
-
-		var self = this;
-
-		/*
-		this.listenTo(this._dataChannel, "peinfo", function(data) {
-
-			var evlists = data.peinfo.evl,
-				seviews = data.peinfo.sev,
-				items = [];
-
-			// Create the start event
-			// self.createStartEvent(seviews);
-			// self.eventCollection.getStartEvent(seviews);
-
-			if (evlists.length <= 0) {
-				return;
-			}
-
-			// Create views from first entry
-			//
-			var peviews = evlists[0].peviews;
-			// loop through the info and create the view entries
-			for (var vi = 0; vi < peviews.length; vi++) {
-				items.push(peviews[vi].viewstate);
-			}
-			self.viewCollection.reset(items);
-
-			// Create the PEs (pei = present event index)
-			items = [ self.eventCollection.getStartEvent(seviews) ];
-
-			for (var pei = 0; pei < evlists.length; pei++) {
-				items.push(evlists[pei]);
-				// self.eventCollection.add(evlists[pei]);
-			}
-			self.eventCollection.reset(items);
-
-		});
-		*/
-
-		/*
-		this.listenTo(this._appChannel, "view:add", function() {
-
-			if ( ! self.viewCollection.canAddView()) {
-				this._appChannel.trigger("ui:error", {
-					message: "Maximum number of views has been reached."
-				});
-				return;
-			}
-
-			// Create the new view now we think we can make it
-			var newView = self.viewCollection.createNew();
-			if (newView) {
-				// Trigger event to say a new view has been added.
-				// Event listeners to this should make sure the views are added to the presentevents and are then updated over IO.
-				self._appChannel.trigger("view:added", {
-					view: newView
-				});
-			} else {
-				self._appChannel.trigger("ui:error", {
-					message: "Unable to create new view."
-				});
-			}
-		});
-		*/
-
+	getPlayMode: function() {
+		return this.playMode
 	}
-
-	/*
-	setupLayers: function() {
-
-		var self = this;
-
-		// Wait for `layerinfo` data
-		this.listenTo(this._dataChannel, "layerinfo", function(data) {
-
-			// Init layers array again with "none" item first
-			self.layers = [{
-				layerid: "none",
-				layername: "none"
-			}];
-
-			// Add the items from the incoming data
-			Array.prototype.push.apply(self.layers, data.layers);
-
-			// Reset the collection with our updated data
-			self.layerCollection.reset(self.layers);
-		});
-	}
-	*/
 
 });
 
