1bc9634160694cafd8ad53f257faf5ef0af248f6
Craig Rodway <craig@clouddataservice.co.uk>
2017-10-02 14:44:39 +0100

    Code formatting + handler for object clicking.
    When an object is clicked, send socket message to say it has. This will then be interpreted and handled.

--- src/screen.js
+++ src/screen.js
@@ -2,175 +2,209 @@
 var USEIO = true;
 var screenstage = null;
 var screenlayer = null;
-var sw,sh;
+var sw, sh;
+var socket = null;
 var ioqueue = [];
 
-//var serverurl = 'http://127.0.0.1:1337';  
-var serverurl = 'http://'+window.location.hostname+':'+window.location.port;
+//var serverurl = 'http://127.0.0.1:1337';
+var serverurl = 'http://' + window.location.hostname + ':' + window.location.port;
 
 var screenanimlist = new Array();
 
 var screenscaling = 1.0;
 
 function initScreen() {
-	
+
 	var vpw = $(window).width();
 	var vph = $(window).height();
-	$('#container').css({'height': Math.round(1.0*vph).toString() + 'px'});	
-	$('#container').css({'width': Math.round(1.0*vpw).toString() + 'px'});	
+	$('#container').css({ 'height': Math.round(1.0 * vph).toString() + 'px' });
+	$('#container').css({ 'width': Math.round(1.0 * vpw).toString() + 'px' });
 	sw = $('#container').width();
 	sh = $('#container').height();
 
-      screenstage = new Kinetic.Stage({
-       container: "container",
-       width: sw,
-       height: sh
-     });
-        
-		screenlayer = new Kinetic.Layer({name:'screenlayer'});
+	screenstage = new Kinetic.Stage({
+		container: "container",
+		width: sw,
+		height: sh
+	});
 
-		screenstage.on('setupdone',function() { processNextio('setupdone');});		
-		screenstage.on('startdone',function() { processNextio('startdone');});		
-		screenstage.on('playdone',function() { processNextio('playdone');});		
-		
-		addLayerAnimation(screenlayer);
-		screenstage.add(screenlayer); 
+	screenlayer = new Kinetic.Layer({ name: 'screenlayer' });
+
+	screenstage.on('setupdone', function () { processNextio('setupdone'); });
+	screenstage.on('startdone', function () { processNextio('startdone'); });
+	screenstage.on('playdone', function () { processNextio('playdone'); });
+
+	addLayerAnimation(screenlayer);
+	screenstage.add(screenlayer);
 
 }
 
-function addLayerAnimation(animlayer)
-{
-	var anim = new Kinetic.Animation(function(frame) {
+function addLayerAnimation(animlayer) {
+	var anim = new Kinetic.Animation(function (frame) {
 		var stopflag = true;
-		for (var i=0;i<screenanimlist.length;i++) {
-			var action = screenanimlist[i]; 	
+		for (var i = 0; i < screenanimlist.length; i++) {
+			var action = screenanimlist[i];
 			var prop = action.interpolateProp(frame);
 			var obj = action.parentobject;
-			obj.setAttr(action.property,prop);
-			
-			if ((frame.time-action.starttime*1000)>action.animDuration*1000) {  
+			obj.setAttr(action.property, prop);
+
+			if ((frame.time - action.starttime * 1000) > action.animDuration * 1000) {
 				stopflag = stopflag && true;   //if many objects wait until last one is finished
 			}
 			else {
 				stopflag = stopflag && false;
 			}
 		}
-	   if (stopflag == true) {
-	   	this.stop();
-	   	frame.time = 0.0;
-	   	screenstage.fire('playdone');		
+		if (stopflag == true) {
+			this.stop();
+			frame.time = 0.0;
+			screenstage.fire('playdone');
 
-	   }
-	   
-	   
-		
+		}
+
+
+
 	}, animlayer);
-	
-	animlayer.setAttr('animation',anim);
 
+	animlayer.setAttr('animation', anim);
 
 }
 
-function makeScreen(screenstate)
-{
+
+function setupEvents(obj) {
+
+	var eventId = obj.getAttr("event");
+	var hasEvent = (obj && eventId !== undefined && eventId.length);
+	if ( ! hasEvent) {
+		return;
+	}
+
+	obj.on('mouseover', function () {
+		document.body.style.cursor = 'pointer';
+		// cursorstate = 'onobj';
+	});
+
+	obj.on('mouseout', function () {
+		document.body.style.cursor = 'default';
+		// cursorstate = 'free';
+	});
+
+	obj.on('mousedown', function () {
+		var eventId = this.getAttr('event');
+		// console.log("triggering event click ID " + eventId);
+		var msg = JSON.stringify({ command: 'clickEvent', info: eventId });
+		socket.emit('updateEvents', msg);
+	});
+}
+
+
+function makeScreen(screenstate) {
+	// console.log("makeScreen");
+	// console.log(screenstate);
 	screenstage.clear();
 	screenlayer.destroyChildren();
-	
-		
-	screenstate.container = 'container'; 
-	
-	var hwratio = screenstate.screenheight/screenstate.screenwidth;
+
+
+	screenstate.container = 'container';
+
+	var hwratio = screenstate.screenheight / screenstate.screenwidth;
 	var vh = sh;
 	var vw = sw;
-	var vhwratio = vh/vw;
+	var vhwratio = vh / vw;
 	if (vhwratio > hwratio) {
 		var stw = vw;
-		var sth = Math.round(vw*hwratio);
-		screenscaling = screenstate.txscale * vw/screenstate.screenwidth;
-	}
-	else {
+		var sth = Math.round(vw * hwratio);
+		screenscaling = screenstate.txscale * vw / screenstate.screenwidth;
+	} else {
 		var sth = vh;
-		var stw = Math.round(vh/hwratio);
-		screenscaling = vh/screenstate.screenheight;
-		screenscaling = screenstate.txscale *vh/screenstate.screenheight;
+		var stw = Math.round(vh / hwratio);
+		screenscaling = vh / screenstate.screenheight;
+		screenscaling = screenstate.txscale * vh / screenstate.screenheight;
 	}
-	
-		//console.log(screenstate);		
 
-		var objects = screenstate.layerchildren;
-		//console.log(objects);
-		for (var k=0;k<objects.length;k++) {
-			var objstate = objects[k];
-			//console.log(objstate);
-			if (objstate.type == 'Group') {
-				var obj = newgroupobj(false,false,objstate);
-				obj.x(objstate.x*screenscaling);
-				obj.y(objstate.y*screenscaling);
-				obj.scale({x:screenscaling,y:screenscaling});
-				screenlayer.add(obj);	
-				//console.log('added group');
-			}
-			else {
-				var obj = newobj(false,objstate);
-				obj.x(objstate.x*screenscaling);
-				obj.y(objstate.y*screenscaling);
-				obj.scale({x:screenscaling,y:screenscaling});
-				screenlayer.add(obj);
-				//console.log('added obj');
-			}	
+	//console.log(screenstate);
+
+	var objects = screenstate.layerchildren;
+	//console.log(objects);
+	for (var k = 0; k < objects.length; k++) {
+		var objstate = objects[k];
+		// console.log(objstate);
+		if (objstate.type == 'Group') {
+			var obj = newgroupobj(false, false, objstate);
+			obj.x(objstate.x * screenscaling);
+			obj.y(objstate.y * screenscaling);
+			obj.scale({ x: screenscaling, y: screenscaling });
+			screenlayer.add(obj);
+			//console.log('added group');
+		} else {
+			var obj = newobj(false, objstate);
+			obj.x(objstate.x * screenscaling);
+			obj.y(objstate.y * screenscaling);
+			obj.scale({ x: screenscaling, y: screenscaling });
+			screenlayer.add(obj);
+			//console.log('added obj');
+
+			setupEvents(obj);
 		}
-		//add actions for the layer
-		//actions are added to the layer actionlist from where they are called using their id
-		var actions = screenstate.layeractions;
-		//console.log(actions);
-		var actionlist = new Array();
-		for (var m=0;m<actions.length;m++) {
-			var actionstate = actions[m];
-				var parentobject = screenlayer.find('#'+actionstate.parentobjectid)[0];
-				actionstate.parentobject = parentobject;
-				//console.log(actionstate);
-				if (actionstate.actiontype == 'move') {
-					var ss = actionstate.startstate;
-					var es = actionstate.endstate;
-					//console.log(ss,es);
-					actionstate.startstate = {x:ss.x*screenscaling,y:ss.y*screenscaling};
-					actionstate.endstate = {x:es.x*screenscaling,y:es.y*screenscaling};
-					//console.log(actionstate.startstate,actionstate.endstate);
-				}
-				var action = new Action(actionstate.parentobject,actionstate.id,actionstate.actiontype,actionstate.startstate,actionstate.endstate,actionstate.starttime,actionstate.duration);
-				actionlist.push(action);
-		} 
-		//console.log(actionlist);
-		screenlayer.setAttr('actionlist',actionlist); 
-		screenlayer.draw();
+		// if (objstate && objstate.event) {
+		// 	console.log("Found click event on:");
+		// 	console.log(objstate);
+		// }
+	}
+	//add actions for the layer
+	//actions are added to the layer actionlist from where they are called using their id
+	var actions = screenstate.layeractions;
+	// console.log("actions");
+	// console.log(actions);
+	var actionlist = new Array();
+	for (var m = 0; m < actions.length; m++) {
+		var actionstate = actions[m];
+		var parentobject = screenlayer.find('#' + actionstate.parentobjectid)[0];
+		actionstate.parentobject = parentobject;
+		//console.log(actionstate);
+		if (actionstate.actiontype == 'move') {
+			var ss = actionstate.startstate;
+			var es = actionstate.endstate;
+			//console.log(ss,es);
+			actionstate.startstate = { x: ss.x * screenscaling, y: ss.y * screenscaling };
+			actionstate.endstate = { x: es.x * screenscaling, y: es.y * screenscaling };
+			//console.log(actionstate.startstate,actionstate.endstate);
+		}
+		var action = new Action(actionstate.parentobject, actionstate.id, actionstate.actiontype, actionstate.startstate, actionstate.endstate, actionstate.starttime, actionstate.duration);
+		actionlist.push(action);
+	}
+	//console.log(actionlist);
+	screenlayer.setAttr('actionlist', actionlist);
+	screenlayer.draw();
 	screenstage.fire('setupdone');
 }
 
-function findObjActions(obj)
-{
+
 /**
 *finds all actions acting on the object
 */
- var objactions = [];
- var actions = screenlayer.getAttr('actionlist');
- for (var ai=0;ai<actions.length;ai++) {
- 	var act = actions[ai];
- 	if (obj.id() == act.parentobject.id()) {
- 		objactions.push(act);
- 	}
- }
+function findObjActions(obj) {
+	var objactions = [];
+	var actions = screenlayer.getAttr('actionlist');
+	for (var ai = 0; ai < actions.length; ai++) {
+		var act = actions[ai];
+		if (obj.id() == act.parentobject.id()) {
+			objactions.push(act);
+		}
+	}
 	return objactions;
 }
+
 
 function setObjstoStartstate() {
 
 	var objs = screenlayer.getChildren();
-	for (var ch=0;ch<objs.length;ch++) {
+	for (var ch = 0; ch < objs.length; ch++) {
 		var obj = objs[ch];
 		var acts = findObjActions(obj);
 		//arrange into property bins
 		var bins = {};
-		for (var ai = 0;ai<acts.length;ai++) {
+		for (var ai = 0; ai < acts.length; ai++) {
 			var acttype = acts[ai].actiontype;
 			var bintype = bins[acttype];
 			if (bintype == null) {
@@ -183,89 +217,83 @@ function setObjstoStartstate() {
 		var firstactions = [];
 		//find earliest starttime for each actiontype and set startstate to that
 		for (var key in bins) {
-				var acttyplist = bins[key];
-				var sttime = 10000.0;
-				for (var i=0;i<acttyplist.length;i++)
-				{
-					if (acttyplist[i].starttime < sttime) {
-						sttime = acttyplist[i].starttime;
-						var shortindex = i;					
-					} 	
+			var acttyplist = bins[key];
+			var sttime = 10000.0;
+			for (var i = 0; i < acttyplist.length; i++) {
+				if (acttyplist[i].starttime < sttime) {
+					sttime = acttyplist[i].starttime;
+					var shortindex = i;
 				}
-				(acttyplist[shortindex]).settoStartstate();
-				firstactions.push(acttyplist[shortindex]);
-			//console.log(obj.getType(),key,acttyplist[shortindex].property,obj.getAttr(acttyplist[shortindex].property));		
+			}
+			(acttyplist[shortindex]).settoStartstate();
+			firstactions.push(acttyplist[shortindex]);
+			//console.log(obj.getType(),key,acttyplist[shortindex].property,obj.getAttr(acttyplist[shortindex].property));
 		}
-		if (firstactions.length>0) {
+		if (firstactions.length > 0) {
 			//now find earliest of all actions and setstart in case opacity and appear conflict
 			var sttime = 10000.0;
-			for (var j=0;j<firstactions.length;j++)
-			{
+			for (var j = 0; j < firstactions.length; j++) {
 				if (firstactions[j].starttime < sttime) {
 					sttime = firstactions[j].starttime;
-					var shortindex = j;					
-				} 	
-			}	
+					var shortindex = j;
+				}
+			}
 			(firstactions[shortindex]).settoStartstate();
-			screenlayer.draw();		
+			screenlayer.draw();
 		}
 	}
 	screenstage.fire('startdone');
 }
 
 
-function play()
-{
-/**
-* Plays actions as cast to the screen
-*/	
+function play() {
+	/**
+	* Plays actions as cast to the screen
+	*/
 	screenanimlist = screenlayer.getAttr('actionlist');
 	//console.log(screenanimlist);
 	var anim = screenlayer.getAttr('animation');
 	screenlayer.draw();
-	anim.start();	
-	
+	anim.start();
+
 
 }
 
-function selectLayer(layername)
-{
+function selectLayer(layername) {
 	screenstage.clear();
-	screenlayer = screenstage.find('.'+layername)[0];
+	screenlayer = screenstage.find('.' + layername)[0];
 	screenlayer.draw();
 	//console.log(screenlayer);
 }
 
 
 function ioUpdate(respdata) {
-		//console.log(respdata);
-		var viewcommand = JSON.parse(respdata);	
-		var command = viewcommand.command;
-		
-		//console.log(command);
-		switch(command) {
-			case 'update':
-				var viewstate = viewcommand.info;
-				makeScreen(viewstate);
+	//console.log(respdata);
+	var viewcommand = JSON.parse(respdata);
+	var command = viewcommand.command;
+
+	// console.log(command);
+	switch (command) {
+		case 'update':
+			var viewstate = viewcommand.info;
+			makeScreen(viewstate);
 			break;
 
-			case 'start':
-				setObjstoStartstate();
-			
+		case 'start':
+			setObjstoStartstate();
+
 			break;
 
-			case 'play':
-				play();
-			
+		case 'play':
+			play();
 			break;
 
-		}
-		
+	}
+
 }
 
-function processNextio(ev)
-{
-	if (ioqueue.length>0) {
+function processNextio(ev) {
+	if (ioqueue.length > 0) {
 		var msg = ioqueue.shift();
 		ioUpdate(msg);
 	}
@@ -274,20 +302,19 @@ function processNextio(ev)
 $(document).ready(function () {
 	initScreen();
 
-	if (USEIO)
-	{  var socket = io(serverurl);
-  		socket.on(socketmessage, function (respdata) {
-  	 		if (ioqueue.length==0) {
-  	 			ioqueue.push(respdata);
-  	 			processNextio('incoming');
-  	 		}
-  	 		else {
-  	 			ioqueue.push(respdata);
-  	 		}
-  	 		
-  		});
-  	}
+	if (USEIO) {
+		socket = io(serverurl);
+		socket.on(socketmessage, function (respdata) {
+			if (ioqueue.length == 0) {
+				ioqueue.push(respdata);
+				processNextio('incoming');
+			}
+			else {
+				ioqueue.push(respdata);
+			}
+		});
+	}
 
 
-	
+
 });
