e05c4381a343002adf75dca1b4060d0f04e5272e
Craig Rodway <craig@clouddataservice.co.uk>
2018-01-15 10:49:43 +0000

    Fixed issue with POST processing in Express

--- src/routes/addproject.js
+++ src/routes/addproject.js
@@ -4,69 +4,84 @@ var qs = require("querystring"),
 exports.method = "post";
 exports.route = "/addproject";
 
-exports.handler = function (req, res) {
+exports.handler = function(req, res) {
 
-	var body = '';
+	var decodedBody = req.body;
+	var pid = decodedBody.id;
+	var pname = decodedBody.name;
+	var pcdate = decodedBody.cdate;
+	var pldate = decodedBody.ldate;
+	var pcreator = decodedBody.creator;
+	var pstate = decodedBody.state;
 
-	req.on('data', function (data) {
-		body += data;
-		// console.log("Partial body: " + body);
-	});
+	/*  	  var pid = (url.parse(req.url,true)).query.id;
+	var pname = (url.parse(req.url,true)).query.name;
+	var pcdate = (url.parse(req.url,true)).query.cdate;
+	var pldate = (url.parse(req.url,true)).query.ldate;
+	var pcreator = (url.parse(req.url,true)).query.creator;
+	var pstate = (url.parse(req.url,true)).query.state;
+	*/
+	var insid = 0;
+	// var db = new sqlite3.Database(dbfile);
+	var db = database.getDb();
+	//check if project exists
+	var sql = "SELECT name,createdate,lastdate,creator,json FROM Projects WHERE id = $id";
+	var params = { $id: pid };
 
-	req.on('end', function () {
-		var decodedBody = qs.parse(body);
-		var pid = decodedBody.id;
-		var pname = decodedBody.name;
-		var pcdate = decodedBody.cdate;
-		var pldate = decodedBody.ldate;
-		var pcreator = decodedBody.creator;
-		var pstate = decodedBody.state;
+	db.each(sql, params, function (error, row) {
+		// console.log(row.name);
+	}, function(err, rows) {
 
-		/*  	  var pid = (url.parse(req.url,true)).query.id;
-		var pname = (url.parse(req.url,true)).query.name;
-		var pcdate = (url.parse(req.url,true)).query.cdate;
-		var pldate = (url.parse(req.url,true)).query.ldate;
-		var pcreator = (url.parse(req.url,true)).query.creator;
-		var pstate = (url.parse(req.url,true)).query.state;
-		*/
-		var insid = 0;
-		// var db = new sqlite3.Database(dbfile);
-		var db = database.getDb();
-		//check if project exists
-		var sql = "SELECT name,createdate,lastdate,creator,json FROM Projects WHERE id = ?";
+		if (rows > 0) {
 
-		db.each(sql, [pid], function (error, row) {
-			//console.log(row.name);
-		}, function(err, rows) {
+			sql = "UPDATE Projects SET name = $name, createdate = $createdate, lastdate = $lastdate, creator = $creator, json = $state WHERE id = $id";
 
-			//console.log(rows);
-			if (rows>0) {
+			params = {
+				$name: pname,
+				$createdate: pcdate,
+				$lastdate: pldate,
+				$creator: pcreator,
+				$state: pstate,
+				$id: pid
+			};
 
-				sql = "UPDATE Projects SET name='"+pname+"',createdate='"+pcdate+"',lastdate='"+pldate+"',creator='"+pcreator+"',json='"+pstate+"' WHERE id='"+pid+"'";
-				//console.log(sql);
-				db.run(sql, function (error) {
-					insid = pid;
-					// db.close();
+			db.run(sql, params, function(error) {
+
+				if (error) {
+					return res.status(500).send({ reason: error });
+				}
+
+				insid = pid;
+				res.send(insid.toString());
+			});
+
+		} else {
+
+			sql = "INSERT INTO Projects (name, createdate, lastdate, creator, json) VALUES ($name, $createdate, $lastdate, $creator, $state)";
+
+			params = {
+				$name: pname,
+				$createdate: pcdate,
+				$lastdate: pldate,
+				$creator: pcreator,
+				$state: pstate
+			};
+
+			db.run(sql, params, function(error) {
+
+				if (error) {
+					return res.status(500).send({ reason: error });
+				}
+
+				db.each("SELECT last_insert_rowid() AS id FROM Projects", function(err, row) {
+					insid = row.id;
+				}, function(err, rows) {
 					res.send(insid.toString());
 				});
 
-			} else {
+			});
+		}
 
-				sql = "INSERT INTO Projects (name,createdate,lastdate,creator,json) VALUES ('"+pname+"','"+pcdate+"','"+pldate+"','"+pcreator+"','"+pstate+"')";
-				db.run(sql, function (error) {
-
-					db.each("SELECT last_insert_rowid() AS id FROM Projects", function(err, row) {
-						insid = row.id;
-					}, function(err, rows) {
-						// db.close();
-						res.send(insid.toString());
-					});
-
-				});
-			}
-
-		});
 	});
-	//        res.writeHead(200, {'Content-Type': 'text/html'});
 
 };
--- src/routes/saveplaylist.js
+++ src/routes/saveplaylist.js
@@ -10,60 +10,50 @@ exports.route = "/saveplaylist";
 
 exports.handler = function(req, res) {
 
-	var body = '';
+	var decodedBody = req.body;
+	//console.log("Body: " + body);
+	var fname = decodedBody.projectname;
+	var playstate = decodedBody.playlist;
+	var playobj = JSON.parse(playstate);
+	var imglist = playobj.playimages;
 
-	req.on('data', function (data) {
-		body += data;
-		// console.log("Partial body: " + body);
-	});
+	console.log(fname,imglist);
+	// 		var imglistobj = JSON.parse(imglist);
+	//console.log(playstate);
+	//serveraddr = getIPAddress();
+	//
+	var htmlstr = '<!DOCTYPE HTML><html><head>';
+	htmlstr = htmlstr + '<meta name="viewport" content="width=device-width, initial-scale=1" />';
+	htmlstr = htmlstr + '<link rel="stylesheet" href="../../playscreen.css">';
+	htmlstr = htmlstr + '<script src="../../jquery-2.1.1.min.js"></script>';
+	htmlstr = htmlstr + '<script src="../../kinetic-v5.1.0.min.js"></script>';
+	htmlstr = htmlstr + '<script src="../../socket.io/socket.io.js"></script>';
 
-	req.on('end', function () {
-		var decodedBody = qs.parse(body);
-		//console.log("Body: " + body);
-		var fname = decodedBody.projectname;
-		var playstate = decodedBody.playlist;
-		var playobj = JSON.parse(playstate);
-		var imglist = playobj.playimages;
+	htmlstr = htmlstr + '<script type="text/javascript" >';
+	htmlstr = htmlstr + 'peinfo='+playstate+';';
+	htmlstr = htmlstr + '</script></head>';
+	htmlstr = htmlstr + '<script src="../../playscreen.js"></script>';
+	htmlstr = htmlstr + '<body><div id="page">';
+	htmlstr = htmlstr + '<div id="functionbox"><input id="playaction" type="checkbox" onchange="setPlaymode()" >play mode<button  type="button" id="nextbutton" onclick="gonextEvent()">next</button></div>';
+	htmlstr = htmlstr + '<div id="playspace"></div></div></body></html>';
+	htmlstr = htmlstr + '<script> $( document ).ready(setup());</script>';
 
-		console.log(fname,imglist);
-		// 		var imglistobj = JSON.parse(imglist);
-		//console.log(playstate);
-		//serveraddr = getIPAddress();
-		//
-		var htmlstr = '<!DOCTYPE HTML><html><head>';
-		htmlstr = htmlstr + '<meta name="viewport" content="width=device-width, initial-scale=1" />';
-		htmlstr = htmlstr + '<link rel="stylesheet" href="../../playscreen.css">';
-		htmlstr = htmlstr + '<script src="../../jquery-2.1.1.min.js"></script>';
-		htmlstr = htmlstr + '<script src="../../kinetic-v5.1.0.min.js"></script>';
-		htmlstr = htmlstr + '<script src="../../socket.io/socket.io.js"></script>';
+	var baseDir = fs.realpathSync(__dirname + "/../data/playlists/");
 
-		htmlstr = htmlstr + '<script type="text/javascript" >';
-		htmlstr = htmlstr + 'peinfo='+playstate+';';
-		htmlstr = htmlstr + '</script></head>';
-		htmlstr = htmlstr + '<script src="../../playscreen.js"></script>';
-		htmlstr = htmlstr + '<body><div id="page">';
-		htmlstr = htmlstr + '<div id="functionbox"><input id="playaction" type="checkbox" onchange="setPlaymode()" >play mode<button  type="button" id="nextbutton" onclick="gonextEvent()">next</button></div>';
-		htmlstr = htmlstr + '<div id="playspace"></div></div></body></html>';
-		htmlstr = htmlstr + '<script> $( document ).ready(setup());</script>';
-
-		var baseDir = fs.realpathSync(__dirname + "/../data/playlists/");
-
-		fs.mkdir(path.join(baseDir, fname), function(err) {
-			if (err) {
-				if (err.code == 'EEXIST') {
-					// ignore the error if the folder already exists
-					helpers.writePlayfileandImages(fname,htmlstr,imglist);
-				} else {
-					console.log(err); // something else went wrong
-				}
-			} else {
-				// successfully created folder
+	fs.mkdir(path.join(baseDir, fname), function(err) {
+		if (err) {
+			if (err.code == 'EEXIST') {
+				// ignore the error if the folder already exists
 				helpers.writePlayfileandImages(fname,htmlstr,imglist);
+			} else {
+				console.log(err); // something else went wrong
 			}
-		});
+		} else {
+			// successfully created folder
+			helpers.writePlayfileandImages(fname,htmlstr,imglist);
+		}
 	});
 
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.send('post received');
+	return res.send("post received");
 
 };
