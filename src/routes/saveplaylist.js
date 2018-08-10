bdf19bafb1b00073f21a34c7302afd890690da1b
Craig Rodway <craig@clouddataservice.co.uk>
2018-08-10 14:00:31 +0100

    Updated Playlist play page template

--- src/routes/saveplaylist.js
+++ src/routes/saveplaylist.js
@@ -2,6 +2,7 @@ var url = require("url"),
 	qs = require("querystring"),
 	fs = require("fs"),
 	path = require("path"),
+	nunjucks = require("nunjucks"),
 	database = require("../includes/database.js"),
 	helpers = require("../includes/helpers");
 
@@ -22,21 +23,10 @@ exports.handler = function(req, res) {
 	//console.log(playstate);
 	//serveraddr = getIPAddress();
 	//
-	var htmlstr = '<!DOCTYPE HTML><html><head>';
-	htmlstr = htmlstr + '<meta name="viewport" content="width=device-width, initial-scale=1" />';
-	htmlstr = htmlstr + '<link rel="stylesheet" href="../../playscreen.css">';
-	htmlstr = htmlstr + '<script src="../../jquery-2.1.1.min.js"></script>';
-	htmlstr = htmlstr + '<script src="../../konva.min.js"></script>';
-	htmlstr = htmlstr + '<script src="../../socket.io/socket.io.js"></script>';
 
-	htmlstr = htmlstr + '<script type="text/javascript" >';
-	htmlstr = htmlstr + 'peinfo='+playstate+';';
-	htmlstr = htmlstr + '</script></head>';
-	htmlstr = htmlstr + '<script src="../../playscreen.js"></script>';
-	htmlstr = htmlstr + '<body><div id="page">';
-	htmlstr = htmlstr + '<div id="functionbox"><input id="playaction" type="checkbox" onchange="setPlaymode()" >play mode<button  type="button" id="nextbutton" onclick="gonextEvent()">next</button></div>';
-	htmlstr = htmlstr + '<div id="playspace"></div></div></body></html>';
-	htmlstr = htmlstr + '<script> $( document ).ready(setup());</script>';
+	var htmlstr = nunjucks.render('playlist_template.html', {
+		playstate: playstate
+	});
 
 	var baseDir = fs.realpathSync(__dirname + "/../data/playlists/");
 
