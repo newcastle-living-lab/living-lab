1405cdfefadf9092441fd83173610e3a4c3365a5
Craig Rodway <craig@clouddataservice.co.uk>
2018-12-14 17:34:59 +0000

    Playlists: Added date, sort by date, and ability to delete.

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -1,5 +1,6 @@
 var fs = require("fs"),
-	auth = require("../includes/auth.js");
+	auth = require("../includes/auth.js"),
+	dateFormat = require("dateformat");
 
 exports.method = "get";
 exports.route = "/playlist";
@@ -8,12 +9,19 @@ exports.handler = [auth.ensureLoggedIn(), auth.ensureRole("view"), function(req,
 
 	var baseDir = fs.realpathSync(__dirname + "/../data/playlists/");
 
-	fs.readdir(baseDir, function(error, files) {
+	var files = fs.readdirSync(baseDir)
+		.map(function(v) {
+			var dt = fs.statSync(baseDir + "/" + v).ctime.getTime();
+			return {
+				name: v,
+				created: dt,
+				date: dateFormat(dt, "ddd dS mmm yyyy HH:MM")
+			};
+		})
+		.sort(function(a, b) { return b.created - a.created; });
 
-		res.render('playlist.html', {
-			playlists: files
-		});
-
+	res.render('playlist.html', {
+		playlists: files
 	});
 
 }];
---
+++ src/routes/removeplaylist.js
@@ -0,0 +1,36 @@
+var url = require("url"),
+	qs = require("querystring"),
+	fs = require("fs"),
+	path = require("path"),
+	rimraf = require("rimraf"),
+	helpers = require("../includes/helpers");
+
+exports.method = "post";
+exports.route = "/removeplaylist";
+
+exports.handler = function(req, res) {
+
+	var decodedBody = req.body;
+	var playlistName = decodedBody.playlist;
+
+	if (playlistName.length === 0) {
+		res.send({ "error": "No playlist supplied.", "deleted": false });
+		return;
+	}
+
+	try {
+		var path = fs.realpathSync(__dirname + "/../data/playlists/" + playlistName);
+	} catch (e) {
+		res.send({ "error": "Playlist folder does not exist.", "deleted": false });
+		return;
+	}
+
+	rimraf(path, function(err) {
+		if ( ! err) {
+			res.send({ "error": false, "deleted": true });
+		} else {
+			res.send({ "error": "Could not remove playlist.", "deleted": false });
+		}
+	});
+
+};
