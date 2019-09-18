87a04547a18ef727b37cd01ec82735a1cd3f72cd
Craig Rodway <craig@clouddataservice.co.uk>
2019-09-18 12:31:00 +0100

    Update Settings to use localStorage

--- src/public/components/Settings.js
+++ src/public/components/Settings.js
@@ -16,14 +16,31 @@ function Settings(values, options) {
 Settings.prototype.setupOptions = function(options) {
 
 	var defaults = {
-		useSessionStorage: true,
-		parseUrl: true
+		useLocalStorage: true,
+		parseUrl: true,
+		namespace: this.getDefaultNamespace(),
 	};
 
 	this.options = $.extend(defaults, options);
 }
 
 
+/**
+ * Generate a default namespace for this based on page path.
+ *
+ */
+Settings.prototype.getDefaultNamespace = function() {
+	var path = window.location.pathname,
+		pathparts = path.split('/'),
+		name = pathparts[ pathparts.length-1 ].replace(/\.html/, '');
+	return name;
+}
+
+
+/**
+ * Set the default values as supplied via params.
+ *
+ */
 Settings.prototype.setupValues = function(values) {
 	for (var property in values) {
 		if (values.hasOwnProperty(property)) {
@@ -33,13 +50,28 @@ Settings.prototype.setupValues = function(values) {
 }
 
 
+/**
+ * Generate storage key with the configured namespace.
+ *
+ */
+Settings.prototype.getKey = function(property) {
+	return [this.options.namespace, property].join(':');
+}
+
+
+/**
+ * Load values from localStorage followed by URL params.
+ *
+ */
 Settings.prototype.loadValues = function() {
 
 	// Load from sessionstroage
 	for (var property in this.store) {
 
-		if (this.options.useSessionStorage && sessionStorage.getItem(property)) {
-			this.setQuietly(property, sessionStorage.getItem(property));
+		var storageKey = this.getKey(property);
+
+		if (this.options.useLocalStorage && localStorage.getItem(storageKey)) {
+			this.setQuietly(property, localStorage.getItem(storageKey));
 		}
 
 		if (this.options.parseUrl && this.urlParams.has(property)) {
@@ -51,6 +83,10 @@ Settings.prototype.loadValues = function() {
 }
 
 
+/**
+ * Get a value from the local storge (already populated from persistent)
+ *
+ */
 Settings.prototype.get = function(key, defaultValue) {
 	if (this.store.hasOwnProperty(key)) {
 		return this.store[key];
@@ -58,6 +94,11 @@ Settings.prototype.get = function(key, defaultValue) {
 	return defaultValue;
 }
 
+
+/**
+ * Set the value locally but don't persist.
+ *
+ */
 Settings.prototype.setQuietly = function(key, value) {
 
 	if (value === 'true') {
@@ -77,6 +118,10 @@ Settings.prototype.setQuietly = function(key, value) {
 }
 
 
+/**
+ * Set a setting value and save it.
+ *
+ */
 Settings.prototype.set = function(key, value) {
 	this.setQuietly(key, value);
 	this.persistStore();
@@ -84,15 +129,20 @@ Settings.prototype.set = function(key, value) {
 }
 
 
+/**
+ * Save the store data keys+values to storage.
+ *
+ */
 Settings.prototype.persistStore = function() {
 
-	if ( ! this.options.useSessionStorage) {
+	if ( ! this.options.useLocalStorage) {
 		return;
 	}
 
 	for (var property in this.store) {
-		console.log("Storing " + property + " value of " + this.store[property]);
-		sessionStorage.setItem(property, this.store[property]);
+		var storageKey = this.getKey(property);
+		console.log("Storing " + storageKey + " value of " + this.store[property]);
+		localStorage.setItem(storageKey, this.store[property]);
 	}
 
 	return this;
